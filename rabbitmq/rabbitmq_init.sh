ef7882f577214dfbb3bec1eafcb2c8127469a86a
mohan.jagabattula <mohan.jagabattula@ril.com>
2019-01-16 21:05:41 +0530

    rabbitmq fix

--- rabbitmq/rabbitmq_init.sh
+++ rabbitmq/rabbitmq_init.sh
@@ -2,11 +2,18 @@
 
 # Create Rabbitmq user
 ( sleep 5 ; \
+echo "*** Create User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD'. ***" ; \
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
+sleep 5 ; \
+echo "*** set administrator perms for user '$RABBITMQ_USER'. ***" ; \
 rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
 sleep 5 ; \
+echo "*** add a virtual host / ***" ; \
 rabbitmqctl add_vhost / ; \
+sleep 5 ; \
+echo "*** set permissions for User '$RABBITMQ_USER'. ***" ; \
 rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
+sleep 5 ; \
 echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
 echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***" ) &
 
